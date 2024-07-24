6e5a4d6f29630bd24dc86c8bbbf1bf693895e717
veerababu12 <106010757+veerababu12@users.noreply.github.com>
2024-07-23 21:45:42 -0700

    Update h.java

--- h.java
+++ h.java
@@ -6,6 +6,6 @@ class h {
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[])
     {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World);
     }
 }
